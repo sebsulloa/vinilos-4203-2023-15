6a40488536745a9ef775a6635e41db5443534265
Oscar Fonseca <o.fonsecan@uniandes.edu.co>
2023-10-31 21:17:51 -0500

    test(album): add ui test album list state

--- app/src/androidTest/java/com/misw/vinilos/AlbumsListScreenTest.kt
+++ app/src/androidTest/java/com/misw/vinilos/AlbumsListScreenTest.kt
@@ -5,6 +5,8 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.misw.vinilos.data.remote.models.Album
+import com.misw.vinilos.data.remote.models.Track
 import com.misw.vinilos.ui.screens.albums.AlbumsListScreen
 import com.misw.vinilos.viewmodels.AlbumsViewModel
 import io.mockk.every
@@ -53,5 +55,37 @@ class AlbumsListScreenTest {
 
         // Then
         composeTestRule.onNodeWithTag("errorMessage").assertIsDisplayed()
+    }
+
+    @Test
+    fun testAlbumListState() {
+        // Given
+        val mockViewModel = mockk<AlbumsViewModel>(relaxed = true)
+        val album = Album(
+            id = 100,
+            name = "Buscando América",
+            cover = "https://i.pinimg.com/564x/aa/5f/ed/aa5fed7fac61cc8f41d1e79db917a7cd.jpg",
+            releaseDate = "1984-08-01T00:00:00.000Z",
+            description = "Buscando América es el primer álbum de la banda de Rubén Blades y Seis del Solar lanzado en 1984.",
+            genre = "Salsa",
+            recordLabel = "Elektra",
+            tracks = listOf(
+                Track(id = 100, name = "Decisiones", duration = "5:05"),
+                Track(id = 101, name = "Desapariciones", duration = "6:29")
+            ),
+            performers = emptyList()
+        )
+
+        // When
+        every { mockViewModel.albums } returns mutableStateOf(listOf(album))
+        every { mockViewModel.isLoading } returns mutableStateOf(false)
+        every { mockViewModel.hasError } returns mutableStateOf(false)
+
+        composeTestRule.setContent {
+            AlbumsListScreen(viewModel = mockViewModel)
+        }
+
+        // Then
+        composeTestRule.onNodeWithTag("albumList").assertIsDisplayed()
     }
 }
