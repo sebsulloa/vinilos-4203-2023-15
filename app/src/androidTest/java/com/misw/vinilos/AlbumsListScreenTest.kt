eb20e49da969aa2942b8c36aa023f6dc0bca7f2f
Oscar Fonseca <o.fonsecan@uniandes.edu.co>
2023-10-31 21:36:23 -0500

    refactor: implements data factory

--- app/src/androidTest/java/com/misw/vinilos/AlbumsListScreenTest.kt
+++ app/src/androidTest/java/com/misw/vinilos/AlbumsListScreenTest.kt
@@ -5,8 +5,6 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.misw.vinilos.data.remote.models.Album
-import com.misw.vinilos.data.remote.models.Track
 import com.misw.vinilos.ui.screens.albums.AlbumsListScreen
 import com.misw.vinilos.viewmodels.AlbumsViewModel
 import io.mockk.every
@@ -61,20 +59,7 @@ class AlbumsListScreenTest {
     fun testAlbumListState() {
         // Given
         val mockViewModel = mockk<AlbumsViewModel>(relaxed = true)
-        val album = Album(
-            id = 100,
-            name = "Buscando América",
-            cover = "https://i.pinimg.com/564x/aa/5f/ed/aa5fed7fac61cc8f41d1e79db917a7cd.jpg",
-            releaseDate = "1984-08-01T00:00:00.000Z",
-            description = "Buscando América es el primer álbum de la banda de Rubén Blades y Seis del Solar lanzado en 1984.",
-            genre = "Salsa",
-            recordLabel = "Elektra",
-            tracks = listOf(
-                Track(id = 100, name = "Decisiones", duration = "5:05"),
-                Track(id = 101, name = "Desapariciones", duration = "6:29")
-            ),
-            performers = emptyList()
-        )
+        val album = DataFactory.createAlbum()
 
         // When
         every { mockViewModel.albums } returns mutableStateOf(listOf(album))
