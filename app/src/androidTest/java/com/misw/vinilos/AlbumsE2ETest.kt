59559c9437ec4796ae060214a04c63cf5c715a3f
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2023-11-16 00:11:18 -0500

    test(album): add e2e testCreateAlbum

--- app/src/androidTest/java/com/misw/vinilos/AlbumsE2ETest.kt
+++ app/src/androidTest/java/com/misw/vinilos/AlbumsE2ETest.kt
@@ -2,14 +2,15 @@ package com.misw.vinilos
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextEquals
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.misw.vinilos.pages.CreateAlbumPage
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
@@ -18,15 +19,39 @@ class AlbumsE2ETest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
+    private val createAlbumPage by lazy { CreateAlbumPage(composeTestRule) }
+
     @Test
     fun testClickAlbumsNavigationItem() {
+        // When
         composeTestRule.onNodeWithTag("AlbumsNavItem").performClick()
+
+        // Then
         composeTestRule.onNodeWithTag("topAppBarTitle").assertTextEquals("Albums")
     }
 
     @Test
     fun testAlbumsLoading() {
+        // When
         composeTestRule.onNodeWithTag("AlbumsNavItem").performClick()
+
+        // Then
         composeTestRule.onNodeWithTag("Loading").assertIsDisplayed()
+    }
+
+    @Test
+    fun testCreateAlbum() {
+        // Given
+        val randomAlbum = DataFactory.createAlbumForm()
+
+        // When
+        with(createAlbumPage) {
+            navigateToAlbumCreationScreen()
+            fillAlbumFormWith(randomAlbum)
+            submitAlbumForm()
+        }
+
+        // Then
+        createAlbumPage.verifyAlbumCreationSuccess()
     }
 }
