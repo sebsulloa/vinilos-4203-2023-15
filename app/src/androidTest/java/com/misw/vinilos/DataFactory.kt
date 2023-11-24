389df8b21a60681d9e82c094014d8145cec7cf92
Sebastian Ulloa <js.ulloa1943@uniandes.edu.co>
2023-11-24 17:00:12 -0500

    fix: issues with E2E tests

--- app/src/androidTest/java/com/misw/vinilos/DataFactory.kt
+++ app/src/androidTest/java/com/misw/vinilos/DataFactory.kt
@@ -67,21 +67,4 @@ object DataFactory {
             seconds = faker.random.nextInt(0, 60)
         )
     }
-
-    fun createAlbumForm(): Album {
-        val recordLabels = listOf("Sony Music", "EMI", "Discos Fuentes", "Elektra", "Fania Records")
-        val genres = listOf("Classical", "Salsa", "Rock", "Folk")
-
-        return Album(
-            id = faker.random.nextInt(intRange = 0..1000),
-            name = faker.music.albums(),
-            cover = "https://source.unsplash.com/random/300x300/?music",
-            releaseDate = faker.random.randomString(length = 15),
-            description = faker.random.randomString(length = 15),
-            genre = genres.random(),
-            recordLabel = recordLabels.random(),
-            tracks = emptyList(),
-            performers = emptyList()
-        )
-    }
 }
---
+++ app/src/androidTest/java/com/misw/vinilos/e2e/AlbumsE2ETest.kt
@@ -0,0 +1,67 @@
+package com.misw.vinilos.e2e
+
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.misw.vinilos.DataFactory
+import com.misw.vinilos.MainActivity
+import com.misw.vinilos.pages.CreateAlbumPage
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import com.misw.vinilos.pages.CreateTrackPage
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class AlbumsE2ETest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    private val createAlbumPage by lazy { CreateAlbumPage(composeTestRule) }
+    private val createTrackPage by lazy { CreateTrackPage(composeTestRule) }
+
+    @Test
+    fun testClickAlbumsNavigationItem() {
+        // When
+        composeTestRule.onNodeWithTag("AlbumsNavItem").performClick()
+
+        // Then
+        composeTestRule.onNodeWithTag("topAppBarTitle").assertTextEquals("Albums")
+    }
+
+    @Test
+    fun testCreateAlbum() {
+        // Given
+        val randomAlbum = DataFactory.createAlbumForm()
+
+        // When
+        with(createAlbumPage) {
+            navigateToAlbumCreationScreen()
+            fillAlbumFormWith(randomAlbum)
+            submitAlbumForm()
+        }
+
+        // Then
+        createAlbumPage.verifyAlbumCreationSuccess()
+    }
+
+    @Test
+    fun testClickFirstAlbumInList() {
+        // Given
+        val randomTrack = DataFactory.createTrackForm()
+
+        // When
+        with(createTrackPage) {
+            navigateToTrackCreationScreen()
+            fillTrackFormWith(randomTrack)
+            submitTrackForm()
+        }
+
+        // Then
+        createTrackPage.verifyTrackCreationSuccess()
+    }
+}
